9dd86c0d79eefeb6e8662b21284530044204b230
fajrh <fajr.habib12@gmail.com>
2025-11-09 06:36:18 -0500

    react
    l

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // The development server is no longer the primary way to run the app,
-  // but this config is still used by `npm run build`.
-  // The proxy has been removed as it's not needed for the single-server setup.
+  plugins: [react()]
 })
